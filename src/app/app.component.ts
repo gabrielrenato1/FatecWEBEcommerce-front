734189a467e71efdd5cbcdea8e58aae76946f05d
Gabriel Renato <gabrielrenatosc@gmail.com>
2024-11-10 02:41:41 -0300

    toggle menu and responsible

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,4 +10,26 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'project';
+  isOpenMenu = false;
+
+   toggleMenu(){
+
+    var navItem = document.querySelector("nav");
+    var contentItem = document.getElementById('content-wrap');
+
+    if(navItem !== null && contentItem !== null){
+    
+      if(window.matchMedia("(orientation: portrait)").matches){
+        navItem.style.left = this.isOpenMenu ? "-86vw" : "0vw";
+      }else{
+        navItem.style.left = this.isOpenMenu ? "-15vw" : "0vw";
+        contentItem.style.width = this.isOpenMenu ? "100vw" : "85vw";
+        contentItem.style.marginLeft = this.isOpenMenu ? "0px" : "15vw";
+      }
+      
+      this.isOpenMenu = !this.isOpenMenu;
+    
+    }
+
+  }
 }
